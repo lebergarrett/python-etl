9ff3f727f444e4ee41fdaa2fbbe475e47cb7a22f
imkumpy <lebergarrett@gmail.com>
2021-05-22 15:11:34 +0000

    Refactored, added transformations file

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 import pandas as pd
-import requests
-import datetime
+import transformation
 
 # URL where the covid data is stored
 nyt_data_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
-# Create a 'dataframe' from csv at dst url (basically a table)
-nyt_df = pd.read_csv(nyt_data_url)
-# Turn the date field from a string into datetime
-nyt_df['date'] = pd.to_datetime(nyt_df.date)
 
-# URL where the John Hopkins data is stored (for recoveries)
+nyt_df = transformation.load_df(nyt_data_url)
+
+# URL where the Johns Hopkins data is stored (for recoveries)
 jh_data_url = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
-# Create a dataframe from csv at dst
-jh_df = pd.read_csv(jh_data_url, usecols=['Date', 'Country/Region', 'Recovered'])
-# Rename Date column to date so it matches nyt df
-jh_df.rename(columns={"Date": "date"}, inplace=True)
-# Turn the date field from a string into datetime
-jh_df['date'] = pd.to_datetime(jh_df.date)
-# Filter dataframe to just US
-jh_df = jh_df.loc[jh_df['Country/Region'] == 'US']
+
+jh_df = transformation.load_df(jh_data_url)
+jh_df = transformation.keep_columns(jh_df, ['date', 'Country/Region', 'Recovered'])
+jh_df = transformation.filter_df(jh_df, 'Country/Region', 'US')
 
 # Merge tables (dataframes) based on matching dates
-merged_df = pd.merge(nyt_df, jh_df, on='date')
-# Drop the Country/Region column
-merged_df = merged_df.drop(columns=['Country/Region'])
-# Change the Recovered column datatype to int, it starts as float for some reason
-merged_df = merged_df.astype({"Recovered": int})
+merged_df = transformation.merge_dfs(nyt_df, jh_df, 'date')
 
 print(merged_df)
 merged_df.to_csv(r'merged_df.csv', index = False)
---
+++ transformation.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def load_df(url):
+    # loads csv into df, but also cleans 'date' column name
+    df = pd.read_csv(url)
+
+    if 'Date' in df.columns:
+        df.rename(columns={"Date": "date"}, inplace=True)
+    
+    df['date'] = pd.to_datetime(df.date)
+    return df
+
+def keep_columns(df, cols):
+    # cols should be a list of columns to keep
+    df = df[cols]
+    return df
+
+def filter_df(df, col, value):
+    # value should be something in the column
+    df = df.loc[df[col] == value]
+    return df
+
+def merge_dfs(df1, df2, key):
+    df = pd.merge(df1, df2, on=key)
+
+    if 'Country/Region' in df.columns:
+        df = df.drop(columns=['Country/Region'])
+    if 'Recovered' in df.columns:
+        df.rename(columns={"Recovered": "recovered"}, inplace=True)
+        df = df.astype({"recovered": int})
+
+    return df
