ca01397e4f975dc9b35b87ab0b917884320c256c
lebergarrett <lebergarrett@gmail.com>
2022-03-30 10:05:43 -0700

    Cleaning code

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import pandas as pd
 import transformation
 
 # URL where the covid data is stored
-nyt_data_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
-
-nyt_df = transformation.load_df(nyt_data_url)
+nyt_data_url='https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+nyt_df=transformation.load_df(nyt_data_url)
 
 # URL where the Johns Hopkins data is stored (for recoveries)
-jh_data_url = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
-
-jh_df = transformation.load_df(jh_data_url)
-jh_df = transformation.keep_columns(jh_df, ['date', 'Country/Region', 'Recovered'])
-jh_df = transformation.filter_df(jh_df, 'Country/Region', 'US')
+jh_data_url='https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
+jh_df=transformation.load_df(jh_data_url)
+jh_df=transformation.keep_columns(jh_df, ['date', 'Country/Region', 'Recovered'])
+jh_df=transformation.filter_df(jh_df, 'Country/Region', 'US')
 
 # Merge tables (dataframes) based on matching dates
-merged_df = transformation.merge_dfs(nyt_df, jh_df, 'date')
+merged_df=transformation.merge_dfs(nyt_df, jh_df, 'date')
 
 print(merged_df)
 merged_df.to_csv(r'merged_df.csv', index = False)
--- transformation.py
+++ transformation.py
@@ -1,32 +1,35 @@
 import pandas as pd
 
+
 def load_df(url):
     # loads csv into df, but also cleans 'date' column name
-    df = pd.read_csv(url)
-
+    df=pd.read_csv(url)
     if 'Date' in df.columns:
         df.rename(columns={"Date": "date"}, inplace=True)
-    
-    df['date'] = pd.to_datetime(df.date)
+    df['date']=pd.to_datetime(df.date)
+
     return df
+
 
 def keep_columns(df, cols):
     # cols should be a list of columns to keep
-    df = df[cols]
+    df=df[cols]
+
     return df
+
 
 def filter_df(df, col, value):
     # value should be something in the column
-    df = df.loc[df[col] == value]
+    df=df.loc[df[col]==value]
     return df
 
-def merge_dfs(df1, df2, key):
-    df = pd.merge(df1, df2, on=key)
 
+def merge_dfs(df1, df2, key):
+    df=pd.merge(df1, df2, on=key)
     if 'Country/Region' in df.columns:
-        df = df.drop(columns=['Country/Region'])
+        df=df.drop(columns=['Country/Region'])
     if 'Recovered' in df.columns:
         df.rename(columns={"Recovered": "recovered"}, inplace=True)
-        df = df.astype({"recovered": int})
+        df=df.astype({"recovered": int})
 
     return df
